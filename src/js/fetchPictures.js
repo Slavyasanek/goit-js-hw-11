ba5212cd220dacd2d473fe4f0cc2d5021265f289
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-12 20:23:22 +0200

    add fetch function

---
+++ src/js/fetchPictures.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = '34346691-066ec8be9ab970c052f0d3dfc';
+const URL = 'https://pixabay.com/api/';
+let page = 1;
+
+export const fetchPictures = async (title) => {
+    const searchFilters = new URLSearchParams({
+        key: API_KEY,
+        q: title,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: page * 40,
+    })
+    const response = await axios.get(`${URL}?${searchFilters}`);
+    return response.data;
+}
+
+// fetchPictures('puppy').then(r => console.log(r),page +=1);
