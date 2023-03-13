68f0035a74d21b5ace278099a74e2d285a9ff273
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-13 17:47:07 +0200

    add variable

--- src/js/fetchPictures.js
+++ src/js/fetchPictures.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const API_KEY = '34346691-066ec8be9ab970c052f0d3dfc';
 const URL = 'https://pixabay.com/api/';
-let page = 1;
 
-export const fetchPictures = async (title) => {
+export const fetchPictures = async (title, page) => {
     const searchFilters = new URLSearchParams({
         key: API_KEY,
         q: title,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: page * 40,
+        per_page: 40,
+        page: page,
     })
     const response = await axios.get(`${URL}?${searchFilters}`);
     return response.data;
