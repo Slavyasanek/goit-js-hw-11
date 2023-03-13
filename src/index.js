268e4fe2cd5a9370b8f4b95cef9e5a0fb29969b1
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-13 18:09:06 +0200

    add simplelightbox

--- src/index.js
+++ src/index.js
@@ -7,69 +7,78 @@ import { noSuccess, totalHitsSucces, endOfLoad, emptyRequestAlert } from "./js/a
 import { renderPosts } from "./js/renderImg";
 
 let page = 1;
-let simplelightbox;
+let simpleLightBox;
 let totalPages;
 let shouldLoad = true;
 
 export const refs = {
-  gallery: document.querySelector('.gallery'),
-  submitForm: document.querySelector('.search-form'),
-  loadBtn: document.querySelector('load-more'),
+    gallery: document.querySelector('.gallery'),
+    submitForm: document.querySelector('.search-form'),
+    loadBtn: document.querySelector('load-more'),
+}
+
+const addFullSizeImg = () => {
+    simpleLightBox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    }).refresh();
 }
 
 const getImages = (e) => {
-  refs.gallery.innerHTML = "";
-  e.preventDefault();
-  page = 1;
-  const request = refs.submitForm.searchQuery.value.trim();
-  fetchPictures(request, page)
-    .then(r => {
-      if (r.hits.length === 0) {
-        noSuccess();
-      } else {
-        if (request === "") {
-            emptyRequestAlert();
-        } else {
-            totalHitsSucces(r.totalHits);
-        }
-        totalPages = Math.ceil(r.totalHits / 40);
-        console.log(r);
-        const posts = renderPosts(r.hits);
-        refs.gallery.insertAdjacentHTML("beforeend", posts);
-        if (r.total > 40) {
-          shouldLoad = true;
-          window.addEventListener("scroll", throttle(loadMore, 500))
-        }
-      }
-    })
-    .catch(er => {
-      console.log(er);
-    })
+    refs.gallery.innerHTML = "";
+    e.preventDefault();
+    page = 1;
+    const request = refs.submitForm.searchQuery.value.trim();
+    fetchPictures(request, page)
+        .then(r => {
+            if (r.hits.length === 0) {
+                noSuccess();
+            } else {
+                if (request === "") {
+                    emptyRequestAlert();
+                } else {
+                    totalHitsSucces(r.totalHits);
+                }
+                totalPages = Math.ceil(r.totalHits / 40);
+                console.log(r);
+                const posts = renderPosts(r.hits);
+                refs.gallery.insertAdjacentHTML("beforeend", posts);
+                addFullSizeImg();
+                if (r.total > 40) {
+                    shouldLoad = true;
+                    window.addEventListener("scroll", throttle(loadMore, 500))
+                }
+            }
+        })
+        .catch(er => {
+            console.log(er);
+        })
 }
 
 refs.submitForm.addEventListener("submit", getImages);
 
 const loadMore = () => {
-  const heightOfBody = document.body.offsetHeight;
-  const screenHeight = window.innerHeight;
-  const scrolled = window.pageYOffset;
+    const heightOfBody = document.body.offsetHeight;
+    const screenHeight = window.innerHeight;
+    const scrolled = window.pageYOffset;
 
-  const thershold = scrolled + screenHeight;
-  const request = refs.submitForm.searchQuery.value.trim();
+    const thershold = scrolled + screenHeight;
+    const request = refs.submitForm.searchQuery.value.trim();
 
-  if (thershold >= heightOfBody && shouldLoad) {
-    page += 1;
-    fetchPictures(request, page)
-      .then((r) => {
-        if (totalPages < page) {
-          endOfLoad();
-          shouldLoad = false;
-          return;
-        } else {
-          const posts = renderPosts(r.hits);
-          refs.gallery.insertAdjacentHTML("beforeend", posts);
-        }
-      })
-  }
+    if (thershold >= heightOfBody && shouldLoad) {
+        page += 1;
+        fetchPictures(request, page)
+            .then((r) => {
+                if (totalPages < page) {
+                    endOfLoad();
+                    shouldLoad = false;
+                    return;
+                } else {
+                    const posts = renderPosts(r.hits);
+                    refs.gallery.insertAdjacentHTML("beforeend", posts);
+                    addFullSizeImg();
+                }
+            })
+    }
 }
 
