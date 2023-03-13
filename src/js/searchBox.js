aea01b7c324ad5cd6331cd7b7f8c38f6467ff75f
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-13 11:24:16 +0200

    update search.js

--- src/js/searchBox.js
+++ src/js/searchBox.js
@@ -1,58 +1,57 @@
 import { fetchPictures } from "./fetchPictures";
 import Notiflix, { Notify } from 'notiflix';
 
-const refs = {
-    gallery: document.querySelector('.gallery'),
-    submitForm: document.querySelector('.search-form'),
-    loadBtn: document.querySelector('load-more'),
+export const refs = {
+  gallery: document.querySelector('.gallery'),
+  submitForm: document.querySelector('.search-form'),
+  loadBtn: document.querySelector('load-more'),
 }
 
 const renderPosts = (pictures) => {
-    const posts = pictures.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return `<div class="photo-card">
-        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+  const posts = pictures.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="photo-card">
+        <a href="${largeImageURL}" class="img-thumb"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="img-main"/></a>
         <div class="info">
           <p class="info-item">
-            <b>Likes: ${likes}</b>
+          Likes: ${likes}
           </p>
           <p class="info-item">
-            <b>Views ${views}</b>
+            Views ${views}
           </p>
           <p class="info-item">
-            <b>Comments: ${comments}</b>
+            Comments: ${comments}
           </p>
           <p class="info-item">
-            <b>Downloads: ${downloads}</b>
+            Downloads: ${downloads}
           </p>
         </div>
       </div>`
-    }).join("")
-    return posts;
+  }).join("")
+  return posts;
 }
 
 const getImages = (e) => {
-    refs.gallery.innerHTML = "";
-    e.preventDefault();
-    const request = refs.submitForm.searchQuery.value;
-    fetchPictures(request)
-        .then(r => {
-            if (r.hits.length === 0) {
-                Notiflix.Notify.failure(
-                    'Sorry, there are no images matching your search query. Please try again.', 
-                    {
-                        timeout: 2000,
-                        cssAnimationStyle: 'zoom',
-                    });
-            } else {
-                const posts = renderPosts(r.hits);
-                page = 1;
-                refs.gallery.insertAdjacentHTML("beforeend", posts);
-            }
-        })
-        .catch(er => {
-            console.log(er);
-        })
-
+  refs.gallery.innerHTML = "";
+  e.preventDefault();
+  const request = refs.submitForm.searchQuery.value;
+  fetchPictures(request)
+    .then(r => {
+      if (r.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            timeout: 2000,
+            cssAnimationStyle: 'zoom',
+          });
+      } else {
+        const posts = renderPosts(r.hits);
+        page = 1;
+        refs.gallery.insertAdjacentHTML("beforeend", posts);
+      }
+    })
+    .catch(er => {
+      console.log(er);
+    })
 }
 
 refs.submitForm.addEventListener("submit", getImages);
